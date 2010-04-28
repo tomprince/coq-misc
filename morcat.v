a59685107313737a11371ba446f4d92d227f5d15
Tom Prince <tom.prince@ualberta.net>
2010-04-28 13:07:55 -0400

    Square.

--- morcat.v
+++ morcat.v
@@ -5,6 +5,8 @@ Require Import Setoid.
 
 (* Eelis *)
 Require Import interfaces.abstract_algebra interfaces.functors theory.categories.
+(* me *)
+Require Import square.
 
 Set Automatic Introduction.
 
@@ -19,23 +21,6 @@ Record Object: Type := object {
 
 Implicit Arguments object [[initial] [terminal]].
 
-Class Square {a b x y: A} (i: a⟶b) (p: x⟶y) (f: a⟶x) (g: b⟶y) := square : g◎i = p◎f.
-
-Section comp_square.
-Context `{Square a b u v i h f g} `{Square u v x y h p f' g'}.
-Global Instance: Square i p (f'◎f) (g'◎g).
-Proof.
-red.
-rewrite <- comp_assoc; hyp_rewrite.
-repeat rewrite comp_assoc; hyp_rewrite.
-reflexivity.
-Qed.
-End comp_square.
-Section id_square.
-Context `{i:a⟶b}.
-Global Instance: Square i i cat_id cat_id.
-Proof. red; rewrite id_l, id_r; reflexivity. Qed.
-End id_square.
 
 Record Arrow (i p: Object) : Type := arrow {
   top : initial i ⟶ initial p;
